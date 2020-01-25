5db7e5d6f45ab4e608ad0a4d88c5bee299058835
ceade <chris.eade@gmail.com>
2020-01-25 23:10:53 +0100

    Implementation of block write into a queue
    It's *heavy* to the max... Well, I guess it's a heavy operation.

--- 04-ringbuf.asm
+++ 04-ringbuf.asm
@@ -55,32 +55,71 @@ rbwb_error:
 ; uint16_t rb_writem(uint16_t src, uint16_t count);
 ; write a chunk of bytes to the queue
 rb_writem:
+    ; check if the count is greater than available space
+    call rb_space               ; get the number of free bytes on the queue
+    ld a, l                     ; load the counter from rb_space return
     ld hl, 0x0002               ; prepare hl to extract argument on the stack
     add hl, sp                  ; skip over return address on stack
     ld c, (hl)                  ; load the count argument
-    call rb_space               ; get the number of free bytes on the queue
-    ld a, l                     ; load the counter from rb_space return
     sub c                       ; subtract the desired count from available
     jp c, rbwm_error            ; desired count is greater than available space
+    ; load count and source address arguments
     ld hl, 0x0002               ; prepare hl to extract argument on the stack
     add hl, sp                  ; skip over return address on stack
     ld b, (hl)                  ; load the count argument into b
-    inc hl                      ; skip over C
     inc hl                      ; skip over B
-    ld d, (hl)                  ; load the src address L
+    inc hl                      ; skip over C
+    ld e, (hl)                  ; load the src address L
     inc hl                      ; skip over L
-    ld e, (hl)                  ; load the src address U
-    ld hl, kq_addr
-    ld c, (hl)                  ; load the mask
+    ld d, (hl)                  ; load the src address U
+    push de                     ; save source address
+    ; set up the destination address
+    ld hl, kq_addr              ; load the base of the table
+    ld c, (hl)                  ; load the mask into c
     inc hl                      ; skip over mask
     inc hl                      ; skip over read pointer
     ld a, (hl)                  ; load the write pointer
     inc hl                      ; skip over write pointer
-    ex de, hl                   ; exchange
-    ; TODO: all the hard shit
+    ld e, a                     ; copy the write pointer to de
+    ld d, 0x00                  ; zero upper byte of de
+    add hl, de                  ; add the write pointer to the queue
+    pop de                      ; restore the source address
+    ex de, hl                   ; hl is the source
+rbwm_loop:
+    push af                     ; save the write pointer
+    ld a, (hl)                  ; read the source byte
+    ex de, hl                   ; hl is destination
+    ld (hl), a                  ; write the destination byte
+    dec b                       ; decrement the counter
+    jp z, rbwm_end              ; finished writing
+    inc hl                      ; increment the destination
+    ex de, hl                   ; hl is source
+    inc hl                      ; increment the source
+    pop af                      ; restore the write pointer
+    inc a                       ; increment the write pointer
+    and c                       ; mask it to wrap
+    jp nz, rbwm_loop            ; if it was not reset to zero then loop
+    ; zero the pointers to the beginning of the queue
+    ex de, hl                   ; hl is destination
+    ld hl, kq_addr              ; reset destination
+    inc hl                      ; skip over mask
+    inc hl                      ; skip over read pointer
+    inc hl                      ; skip over write pointer
+    ex de, hl                   ; hl is source
+    jp rbwm_loop                ; continue writing
+rbwm_end:
+    ; save the new write pointer
+    pop af                      ; restore the write pointer
+    inc a                       ; increment the write pointer
+    and c                       ; mask it to wrap
+    ld hl, kq_addr              ; set destination
+    inc hl                      ; skip over mask
+    inc hl                      ; skip over read pointer
+    ld (hl), a                  ; save the incremented write pointer
+    ld hl, 0x0000               ; set success return code
     ret
 rbwm_error:
-    ld hl, 0xffff               ; error value
+    ld hl, 0xffff               ; set error value
     ret
 
 rb_readb:
