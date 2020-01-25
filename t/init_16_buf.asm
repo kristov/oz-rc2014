e0345220a432b4c57795ca0659c45e4f84219848
ceade <chris.eade@gmail.com>
2020-01-25 22:50:02 +0100

    Make queue location more accurate

--- t/init_16_buf.asm
+++ t/init_16_buf.asm
@@ -1,8 +1,8 @@
-kq_addr: equ 0x100          ; location of queue
+kq_addr: equ 0x0100          ; location of queue
 
 init:
     ; set up a 16 byte queue
-    ld sp, 0x0140
+    ld sp, 0x0fff
     ld hl, kq_addr
     ld a, 0x0f
     ld (hl), a
