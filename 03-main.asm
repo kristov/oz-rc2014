c3711044effa378d4c8b47941cc6450ab0c2ab23
ceade <chris.eade@gmail.com>
2020-01-28 22:53:04 +0100

    Test for calling a function via kfn_addr
    The mechanisim for calling the function address for a producer or consumer
    function.

--- 03-main.asm
+++ 03-main.asm
@@ -1,3 +1,36 @@
+; The main OS loop.
+;
+kq_main:
+    ld a, (kq_curr_id)          ; load the current queue idx into a
+    call kqs_next_q_id          ; find the next runnable queue
+    sub l                       ; test equality with returned id
+    jp z, kq_main               ; loop indefinitely unless a new queue found
+    push hl                     ; push the new queue id onto args
+    call kq_run_queue           ; run the newly detected queue
+    pop hl                      ; clean up arguments
+    jp kq_main                  ; do it all over again
+
+; Takes a queue id from argument and run the producer and consumer functions.
+;
+kq_run_queue:
+    ld hl, 0x0003               ; extract argument 2 from stack
+    add hl, sp                  ; skip over return address on stack
+    ld a, (hl)                  ; load the id
+    ; calculate offset in queue table
+    ; copy queue address into kq_addr
+    ; copy producer function into kfn_addr
+    ret
+
+; Run the loaded function.
+;
+kq_run_func:
+    ld hl, (kfn_addr)           ; load the function address into hl
+    ld bc, kq_run_func_ret      ; put return address in bc
+    push bc                     ; push return address on function stack
+    jp (hl)                     ; jump to the function
+kq_run_func_ret:
+    ret
+
 ; Returns the index of the next available queue record.
 ;
 kqs_next_q_id:
--- t/init_kqueue_status.asm
+++ t/init_kqueue_status.asm
@@ -2,4 +2,5 @@ ram_base: equ 0x0300            ; base of ram for kernel vars
 kqs_mask: equ 0x0f              ; 16 byte status table
 
 kq_curr_id: equ ram_base        ; location of current queue idx
-kqs_tbase: equ kq_curr_id + 1   ; location of queue status table
+kfn_addr: equ kq_curr_id + 1    ; function address
+kqs_tbase: equ kfn_addr + 2     ; location of queue status table
