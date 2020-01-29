e5f30796cb505ebe59d5bf2f28eff054764a5698
ceade <chris.eade@gmail.com>
2020-01-29 18:05:17 +0100

    Convert kq_run_func to use stack argument
    Rather than global variable.

--- 03-main.asm
+++ 03-main.asm
@@ -1,30 +1,46 @@
 ; The main OS loop.
 ;
-kq_main:
-    ld a, (kq_curr_id)          ; load the current queue idx into a
-    call kqs_next_q_id          ; find the next runnable queue
-    sub l                       ; test equality with returned id
-    jp z, kq_main               ; loop indefinitely unless a new queue found
-    push hl                     ; push the new queue id onto args
-    call kq_run_queue           ; run the newly detected queue
-    pop hl                      ; clean up arguments
-    jp kq_main                  ; do it all over again
+;kq_main:
+;    ld a, (kq_curr_id)          ; load the current queue idx into a
+;    call kqs_next_q_id          ; find the next runnable queue
+;    sub l                       ; test equality with returned id
+;    jp z, kq_main               ; loop indefinitely unless a new queue found
+;    push hl                     ; push the new queue id onto args
+;    call kq_run_queue           ; run the newly detected queue
+;    pop hl                      ; clean up arguments
+;    jp kq_main                  ; do it all over again
 
 ; Takes a queue id from argument and run the producer and consumer functions.
 ;
-kq_run_queue:
-    ld hl, 0x0003               ; extract argument 2 from stack
-    add hl, sp                  ; skip over return address on stack
-    ld a, (hl)                  ; load the id
-    ; calculate offset in queue table
-    ; copy queue address into kq_addr
-    ; copy producer function into kfn_addr
-    ret
+;kq_run_queue:
+;    ld hl, 0x0003               ; extract argument 2 from stack
+;    add hl, sp                  ; skip over return address on stack
+;    ld d, 0x00                  ; zero d
+;    ld e, (hl)                  ; load the id
+;    ex de, hl                   ; swap them for multiplication
+;    add hl, hl                  ; 2x
+;    add hl, hl                  ; 4x
+;    add hl, hl                  ; 8x
+;    ld de, kq_tbase             ; set the queue table base
+;    add hl, de                  ; add multiplied offset
+;    ld de, kq_addr              ; load base of queue vars
+;    ldi
+;    ldi
+;    ldi
+;    ldi
+;    ldi
+;    ldi
+;    ret
 
-; Run the loaded function.
+; Run the loaded producer function.
 ;
 kq_run_func:
-    ld hl, (kfn_addr)           ; load the function address into hl
+    ld hl, 0x0002               ; extract argument stack
+    add hl, sp                  ; skip over return address on stack
+    ld e, (hl)                  ; load lower byte
+    inc hl                      ; move to higher byte
+    ld d, (hl)                  ; load higher byte
+    ex de, hl                   ; de is nowthe function address
     ld bc, kq_run_func_ret      ; put return address in bc
     push bc                     ; push return address on function stack
     jp (hl)                     ; jump to the function
--- t/init_kqueue_status.asm
+++ t/init_kqueue_status.asm
@@ -2,5 +2,7 @@ ram_base: equ 0x0300            ; base of ram for kernel vars
 kqs_mask: equ 0x0f              ; 16 byte status table
 
 kq_curr_id: equ ram_base        ; location of current queue idx
-kfn_addr: equ kq_curr_id + 1    ; function address
-kqs_tbase: equ kfn_addr + 2     ; location of queue status table
+kq_addr: equ kq_curr_id + 1     ; location of current queue data
+kfn_paddr: equ kq_addr + 2      ; producer function address
+kfn_caddr: equ kfn_paddr + 2    ; consumer function address
+kqs_tbase: equ kfn_caddr + 2    ; location of queue status table
