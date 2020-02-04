be8fd6f92cd6c698584315ab095ff6557b46fb59
ceade <chris.eade@gmail.com>
2020-02-04 22:06:45 +0100

    Work on sets: init an empty set, and append new space

--- 05-queuelist.asm
+++ 05-queuelist.asm
@@ -1,18 +1,71 @@
-; Find the address just after the the last queue
+; Init an empty set at the address passed on the stack. 
 ;
-ql_end_addr:
-    ld hl, (ql_base)            ; load the start of the queue list
-qlea_next:
-    ld d, 0x00                  ; zero high byte of d
-    ld a, (hl)                  ; load the queue mask (length)
-    or a                        ; test for zero
-    jp z, qlea_found            ; end of list
-    ld e, a                     ; copy length over to e
-    inc hl                      ; skip over mask
-    inc hl                      ; skip over read pointer
-    inc hl                      ; skip over write pointer
-    add hl, de                  ; add queue length
-    inc hl                      ; add one
-    jp qlea_next                ; look for the next queue
-qlea_found:
+ql_set_init:
+    ld hl, 0x0002               ; prepare hl to extract argument on the stack
+    add hl, sp                  ; skip over return address on stack
+    ld e, (hl)                  ; load the dest address L
+    inc hl                      ; skip over L
+    ld d, (hl)                  ; load the dest address U
+    ex de, hl                   ; set hl to destination address
+    ld (hl), 0x00               ; load the type L
+    inc hl                      ; move to U
+    ld (hl), 0x80               ; set the bit to represent a set
+    ret
+
+; ql_set_append(set, size);
+;
+;     ld bc, <set address>           ; push the address of the set
+;     ld de, <size of thing to add>  ; push the size of the chunk
+;     call ql_set_append             ; returns the address of where the chunk data starts in hl
+;
+ql_set_append:
+    ld hl, 0x0002               ; prepare hl to extract argument on the stack
+    add hl, sp                  ; skip over return address on stack
+
+    ; load the desired space value into bc and make sure its not too large
+    ld c, (hl)                  ; load the size address L
+    inc hl                      ; skip over L
+    ld b, (hl)                  ; load the size address U
+    bit 7, b                    ; test if the size is too big
+    jp nz, qlsa_error           ; the size is too big
+    inc hl                      ; skip over U
+
+    ; load the set address from stack argument and load the current set size
+    ld e, (hl)                  ; load the set address L
+    inc hl                      ; skip over L
+    ld d, (hl)                  ; load the set address U
+    ex de, hl                   ; set hl to set address, de is stack location
+    ld e, (hl)                  ; load the lower of the size
+    inc hl                      ; skip over L
+    ld a, (hl)                  ; load the U into a
+    ld d, 0x7f                  ; prepare to mask away the set type bit
+    and d                       ; mask away the set type bit
+    ld d, a                     ; de now contains the current size
+    push de                     ; save original size
+
+    ; add the desired size to current size in set and check if too big
+    ex de, hl                   ; de is address of upper byte of set size, hl is current size
+    add hl, bc                  ; hl is the new size
+    jp c, qlsa_pop_error        ; new size too big
+
+    ; write the new size to the set header making sure the set bit is on
+    ex de, hl                   ; de is the new size, hl is is pointing to high byte
+    dec hl                      ; decrement hl to low byte of set size
+    ld a, d                     ; set a to U of new size
+    ld b, 0x80                  ; prepare to set the set bit
+    or b                        ; set the set bit
+    ld d, a                     ; put the modified U back in de
+    ld (hl), e                  ; save the new size L
+    inc hl                      ; skip over L
+    ld (hl), d                  ; save the new size U
+
+    ; add the old set size to the set data address to get end of the set
+    inc hl                      ; hl is now the start of the chunk data
+    pop de                      ; load the old size into de
+    add hl, de                  ; hl is now where the append can happen
+    ret
+qlsa_pop_error:
+    pop de                      ; discard the original size
+qlsa_error:
+    ld hl, 0x0000               ; returns NULL
     ret
